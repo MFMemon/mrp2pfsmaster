f9240639b09ce3490672399d648ef46dff31d114
MFMemon <mohammadfaizan91@gmail.com>
2024-10-18 22:31:59 +0400

    fixed download url

--- runfsserver.sh
+++ runfsserver.sh
@@ -8,15 +8,9 @@ DOWNLOAD_URL=https://github.com/seaweedfs/seaweedfs/releases/download
 rm -f /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
 rm -rf /tmp/seaweedfs && mkdir -p /tmp/seaweedfs
 
-curl -L ${DOWNLOAD_URL}/${SEAWEEDFS_VER}/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz -o /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
+curl -L ${DOWNLOAD_URL}/${SEAWEEDFS_VER}/linux_amd64_full.tar.gz -o /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
 tar xzvf /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz -C /tmp/seaweedfs
 rm -f /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
 
 mkdir /tmp/seaweedfs/masterdir
 /tmp/seaweedfs/weed master -port=$1 -mdir=masterdir &
-
-
-
-
-wget https://github.com/seaweedfs/seaweedfs/releases/download/3.77/linux_amd64_full.tar.gz
-
