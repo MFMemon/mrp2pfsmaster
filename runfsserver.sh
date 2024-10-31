409bcb6ff19c4db75665f3fbd263ebce3561d5b8
MFMemon <mohammadfaizan91@gmail.com>
2024-10-31 11:41:24 +0400

    modified args to deployment script

--- runfsserver.sh
+++ runfsserver.sh
@@ -1,4 +1,4 @@
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl iproute2
 
 SEAWEEDFS_VER=3.77
 
@@ -12,5 +12,8 @@ curl -L ${DOWNLOAD_URL}/${SEAWEEDFS_VER}/linux_amd64_full.tar.gz -o /tmp/seaweed
 tar xzvf /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz -C /tmp/seaweedfs
 rm -f /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
 
+listen_ip = $(ip a | awk "/inet/ && /brd/" | tail -1 | awk '{print $2}' | awk -F/ '{print $1}')
+
 mkdir /tmp/seaweedfs/masterdir
-/tmp/seaweedfs/weed master -port.grpc=$1 -port=$2 -mdir=masterdir
+/tmp/seaweedfs/weed master -mdir=masterdir -peers=$1 -ip=${listen_ip} -defaultReplication=020
+# /tmp/seaweedfs/weed master -port.grpc=$1 -port=$2 -mdir=masterdir
